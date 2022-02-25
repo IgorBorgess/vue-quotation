a02b7e73f861dd6751c056ed009c39fb1839a426
Igor Borges <igorborgeswrk@gmail.com>
2022-02-25 19:13:45 -0300

    [If applied] will create the skeleton while building the code in order to consume the api. It's pretty much done, although a improvement will be made so the user can follow the pair he wants.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container grid-lg my-2 py-2">
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        <ListQuotes :quotes="quotes" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, toRefs } from '@vue/reactivity'
+import api from '@/services/api'
+import ListQuotes from './components/ListQuotes'
+import { onMounted } from '@vue/runtime-core'
 
-export default {
+export default { 
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ListQuotes },
+  setup() {
+
+  const data = reactive({
+      quotes: {}
+    })
+
+    onMounted(async () => {
+      const response = await api.all()
+      data.quotes = response.data
+    })
+    
+
+    return { ...toRefs(data) }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
