14ddde7a702fa93b5c0cf8d52f69c73c2905c1bc
Igor Borges <igorborgeswrk@gmail.com>
2022-02-25 20:43:07 -0300

    [If applied] will build the logic to implement the following functions while creating a table to fit in the selected pairs of coins.

--- src/components/ListQuotes.vue
+++ src/components/ListQuotes.vue
@@ -27,10 +27,18 @@
                 </td>
                 <td>
                     <a 
-                    href="#" 
+                    v-if="!listenQuotes.includes(key)"
                     class="btn btn-primary btn-sm tooltip tooltip-left"
-                    data-tooltip="Seguir">
+                    data-tooltip="Seguir"
+                    @click="$emit('listen', key)">
                     <i class="icon icon-plus"></i>
+                    </a>
+                    <a
+                    v-else 
+                    class="btn btn-error btn-sm tooltip tooltip-left"
+                    data-tooltip="Remover"
+                    @click="$emit('unlisten', key)">
+                    <i class="icon icon-minus"></i>
                     </a>
                 </td>
             </tr>
@@ -40,7 +48,9 @@
 
 <script>
 export default {
-    props: { quotes: { type: Object, required: true }}
+    props: { quotes: { type: Object, required: true },
+            listenQuotes: {type: Array, required: true}},
+    emits: ['listen', 'unlisten']
 }
 </script>
 
